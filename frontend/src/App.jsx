17187dd54c914b5db0c76cb340ba997bf35cebbf
andayray <andre9sumilang@gmail.com>
2025-07-30 20:50:54 -0400

    fixed styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,7 @@
 import PublicChat from './PublicChat.tsx';
+import './index.css';     // For body/html/reset styles
+import './App.css';       // If used for layout or centering
+import './PublicChat.css';
 
 function App() {
     return (
