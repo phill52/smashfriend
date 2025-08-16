ba820b7b32c2957f28deca5c32ce0329f2412a48
Phillip Anerine <paner225@gmail.com>
2025-08-16 16:32:55 -0400

    Add clerk integration to user

--- backend/models/users.go
+++ backend/models/users.go
@@ -1,9 +1,21 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
 
-	Username string `json:"username" gorm:"uniqueIndex;not null"`
+	ClerkID string `json:"clerk_id" gorm:"uniqueIndex;not null"`
+
+	Username    string `json:"username" gorm:"uniqueIndex;not null"`
+	DisplayName string `json:"display_name"`
+
+	ProfilePicture string `json:"profile_picture"`
+
+	IsActive bool      `json:"is_active" gorm:"default:true"`
+	LastSeen *time.Time `json:"last_seen"`
 }
