d68811174abaf7eef21012335d881e01e21727fa
dsr5964 <dsr5964@rit.edu>
2025-08-21 19:19:37 -0400

    Updated changes in code review. Made the classes more clean in profile. Still figuring out how to do custom colors.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,41 @@
 import {
-    SignedIn,
-    SignInButton,
-    SignedOut,
-    SignOutButton,
-    UserButton,
+  SignedIn,
+  SignInButton,
+  SignedOut,
+  SignOutButton,
+  UserButton,
 } from "@clerk/clerk-react";
 
-import "./index.css"
+import "./index.css";
 import "./App.css";
-import Profile from "./components/profile"
-import react_path from './assets/react.svg'
+import Profile from "./components/profile";
+import react_path from "./assets/react.svg";
 
 const user = {
-    user_id: "123456789",
-    image_url: react_path,
-    username: "dan",
-    rank: "Universal",
-    elo_rating: "2500",
-    games_won: 152,
-    games_lost: 53
+  user_id: "123456789",
+  image_url: react_path,
+  username: "dan",
+  rank: "Universal",
+  elo_rating: "2500",
+  games_won: 152,
+  games_lost: 53,
 };
 
 function App() {
-    return (
-        <>
-        <div>
-            <Profile 
-                user={user}
-            />
-        </div>
-
-            <SignedOut>
-                <SignInButton />
-            </SignedOut>
-            <SignedIn>
-                <UserButton />
-                <SignOutButton />
-            </SignedIn>
-        </>
-    );
+  return (
+    <>
+      <>
+        <Profile user={user} />
+      </>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+        <SignOutButton />
+      </SignedIn>
+    </>
+  );
 }
 
 export default App;
--- frontend/src/components/profile.tsx
+++ frontend/src/components/profile.tsx
@@ -1,38 +1,50 @@
-import "./profile.css"
+import "./profile.css";
 
-function ProfileComponent({user}) {
-    const win_rate = Math.floor((user.games_won / (user.games_won + user.games_lost)) * 100)
-    return (
-        <div className="flex justify-end items-center min-h-[calc(100vh-4.75rem)] text-white mr-[0.625rem]">
-            <div className="w-[10.9375rem] h-[28.125rem] border border-[#38334E] text-center bg-[#191E29] rounded-[0.625rem] font-bold p-[0.625rem] text-white mr-[0.625rem]">
-                Your Profile
-                <div className="p-4">
-                    <img className="rounded-full bg-white h-[6.25rem] w-[6.25rem] mx-auto" src={user.image_url} alt="user profile"/>
-                    <div className="pt-[0.3125rem] text-[0.875rem]">
-                        <p>{user.username}</p>
-                        <div className="text-[0.625rem] w-full h-full text-[#CACACA] mb-[0.3125rem]">
-                            <h6>Start gg ID: {user.user_id}</h6>
-                        </div>
-                        <div className="pt-[0.625rem]">
-                            <p className="mb-[0.625rem]">Current Rank</p>
-                            <div className="w-0 h-0 border-l-[2rem] border-r-[1.90625rem] border-t-[3.8125rem] border-l-transparent border-r-transparent border-t-white mx-auto"></div>
-                            <p>{user.rank}</p>
-                            <div className="text-[0.625rem] w-full h-full text-[#CACACA] mb-[0.3125rem]">
-                                <h6>{user.elo_rating} ELO</h6>
-                            </div>
-                            <p>This Season:</p>
-                            <div className="mt-[0.3125rem] whitespace-nowrap text-[0.9375rem]">
-                                <p><span style={{color: "green"}}>{user.games_won} W</span>-<span style={{color: "red"}}> {user.games_lost} L </span>({win_rate}%)</p>
-                            </div>
-                        </div>
-                        <div style={{margin: "1rem"}}>
-                            <button className="bg-[#96619A] rounded-[0.625rem] text-[0.625rem] gap-[0.5rem] font-bold py-[0.5rem] px-[0.75rem] cursor-pointer">Full Profile &gt;</button>
-                        </div>
-                    </div>
-                </div>
+function ProfileComponent({ user }) {
+  const win_rate = Math.floor(
+    (user.games_won / (user.games_won + user.games_lost)) * 100,
+  );
+  return (
+    <div className="flex min-h-[calc(100vh-4.75rem)] w-full justify-end text-white">
+      <div className="mt-1.5 mr-2 rounded-lg border border-[#38334E] bg-[#191E29] p-2.5 text-center font-bold text-white">
+        Your Profile
+        <div className="p-4">
+          <img
+            className="mx-auto h-24 w-24 rounded-full bg-white"
+            src={user.image_url}
+            alt="user profile"
+          />
+          <div className="pt-1.5 text-sm">
+            <p>{user.username}</p>
+            <div className="mb-0.5 text-xs text-[#CACACA]">
+              <h6>Start gg ID: {user.user_id}</h6>
             </div>
+            <div className="pt-1">
+              <p className="m-0.5">Current Rank</p>
+              <div className="m-1.5 mx-auto h-0 w-0 border-t-56 border-r-32 border-l-32 border-t-white border-r-transparent border-l-transparent"></div>
+              <p>{user.rank}</p>
+              <div className="mb-1 text-xs text-[#CACACA]">
+                <h6>{user.elo_rating} ELO</h6>
+              </div>
+              <p>This Season:</p>
+              <div className="mt-1 text-sm whitespace-nowrap">
+                <p>
+                  <span className="text-green-600">{user.games_won} W</span>-
+                  <span className="text-red-600"> {user.games_lost} L </span>(
+                  {win_rate}%)
+                </p>
+              </div>
+            </div>
+            <div>
+              <button className="mt-2 cursor-pointer rounded-lg bg-[#96619A] px-3 py-2 text-sm font-bold">
+                Full Profile &gt;
+              </button>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default ProfileComponent;
