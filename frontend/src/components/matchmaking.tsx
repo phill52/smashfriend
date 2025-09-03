e70d16283685c25251479c40e3dbd19da3f0117d
dsr5964 <dsr5964@rit.edu>
2025-09-03 16:33:14 -0400

    added state for the best of buttons. Now when you click on unranked it renders bo3, bo5, and any buttons that you click on it sets state.

--- frontend/src/components/matchmaking.tsx
+++ frontend/src/components/matchmaking.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 function MatchmakingComponent() {
   const [selectedValue, setSelectedValue] = useState("");
   const [isRanked, setIsRanked] = useState(false);
+  const [bestOf, setBestOf] = useState(null);
 
   const handleChange = (event) => {
     setSelectedValue(event.target.value);
@@ -10,12 +11,15 @@ function MatchmakingComponent() {
 
   const handleModeButtonClick = (isRanked) => {
     setIsRanked(isRanked);
-    console.log(isRanked);
+  };
+
+  const handleBestOfClick = (value) => {
+    setBestOf(value);
   };
 
   return (
     <div className="flex justify-center">
-      <div className="text-white-700 ${isRanked bg-[#171721]} m-auto h-72 w-96 justify-center rounded-md text-center font-bold text-white">
+      <div className="text-white-700 m-auto h-72 w-96 justify-center rounded-md bg-[#171721] text-center font-bold text-white">
         <h1 className="pt-4 text-2xl">Find a Match</h1>
         <h2 className="text-smokey-white">Mode</h2>
         <div className="flex justify-center gap-2 px-4 py-2">
@@ -35,9 +39,36 @@ function MatchmakingComponent() {
         <h2 className="text-smokey-white">Best of</h2>
         <div className="m-auto flex flex-col justify-center">
           <div className="my-3">
-            <button className="bg-hot-pink border-neon-pink m-auto self-start rounded-xl border px-10 py-2 text-xs">
-              Bo3
-            </button>
+            {isRanked && (
+              <button
+                className={`${bestOf === "Bo3" ? "bg-hot-pink border-neon-pink" : "border-[#7165AA] bg-[#201F2f]"} m-auto cursor-pointer self-start rounded-xl border px-10 py-2 text-xs`}
+                onClick={() => handleBestOfClick("Bo3")}
+              >
+                Bo3
+              </button>
+            )}
+            {!isRanked && (
+              <div className="flex">
+                <button
+                  className={`${bestOf === "Bo3" ? "bg-hot-pink border-neon-pink" : "border-[#7165AA] bg-[#201F2f]"} m-auto cursor-pointer self-start rounded-xl border px-10 py-2 text-xs`}
+                  onClick={() => handleBestOfClick("Bo3")}
+                >
+                  Bo3
+                </button>
+                <button
+                  className={`${bestOf === "Bo5" ? "bg-hot-pink border-neon-pink" : "border-[#7165AA] bg-[#201F2f]"} m-auto cursor-pointer self-start rounded-xl border px-10 py-2 text-xs`}
+                  onClick={() => handleBestOfClick("Bo5")}
+                >
+                  Bo5
+                </button>
+                <button
+                  className={`${bestOf === "Any" ? "bg-hot-pink border-neon-pink" : "border-[#7165AA] bg-[#201F2f]"} m-auto cursor-pointer self-start rounded-xl border px-10 py-2 text-xs`}
+                  onClick={() => handleBestOfClick("Any")}
+                >
+                  Any
+                </button>
+              </div>
+            )}
           </div>
           <button className="bg-pink-purple m-auto self-start rounded-xl border border-[#38334E] px-10 py-2 text-xs">
             Queue
